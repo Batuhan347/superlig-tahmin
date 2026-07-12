88dedfc7d516de9154745529324b69033a8a8abf
Batuhan347 <aron93792@gmail.com>
2026-07-12 20:31:24 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Projenize özel Firebase bağlantı bilgileri
 const firebaseConfig = {
   apiKey: "AIzaSyDfRP7uRWDidJ_I0smhkHcXbQr34L_7lv4",
   authDomain: "superlig-tahmin.firebaseapp.com",
@@ -16,7 +15,6 @@ const firebaseConfig = {
   appId: "1:69259937286:web:489b323ab6666e4b238d67"
 };
 
-// Firebase servislerini başlat ve diğer dosyalarda kullanılmak üzere dışa aktar
 export const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
